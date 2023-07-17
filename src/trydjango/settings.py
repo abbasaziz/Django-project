f179e586550764f40b8b7e220990ae1491852d4a
Abbas Aziz <77748051+abbasaziz@users.noreply.github.com>
2023-07-17 21:24:21 +0500

    Update settings.py

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # My Apps
+    # My Apps that i used in the API
     'products',
     'pages',
 ]
